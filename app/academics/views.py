f560d34f3f727715b73eac30b02333794809d161
Hector <lizcanoagudelo@gmail.com>
2024-04-22 11:16:47 -0500

    DJANDO PROJECT

--- app/academics/views.py
+++ app/academics/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 def index(request):
     return HttpResponse("::: Welcome to my site :::")
 
-def list_persons(request):
-    return HttpResponse("Here you find a list of people")
-def create_persons(request):
-    return HttpResponse("Create Persons")
+def list_users(request):
+    #return HttpResponse("Here you find a list of user")
+    users = User.objects.all()
+    return render(request, 'academics/list_users.html', {'users': users})
+def create_user(request):
+    return HttpResponse("Create User")
